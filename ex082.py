045ee505d787f0c6dcf7615fa1a4b295c041b716
Felipe do Nascimento Fonseca <felipenasfonseca@gmail.com>
2021-09-08 23:18:26 -0300

    segundo

--- ex082.py
+++ ex082.py
@@ -6,5 +6,16 @@ lista = []
 pares = []
 impares = []
 while True:
-    lista = int(input('digite um número: '))
-    
+    add = int(input('digite um número: '))
+    lista.append(add)
+    c = str(input('quer continuar?[s/n]: ')).strip().lower()
+    if c in 'n':
+        break
+print(f'os números são {lista}')
+for t, y in enumerate(lista):
+    if y%2==0:
+        pares.append(y)
+    else:
+        impares.append(y)
+print(f'os pares são {pares}')
+print(f'os impares são {impares}')
--- ex083.py
+++ ex083.py
@@ -1,2 +1,16 @@
 # crie um programa onde o usuario digite uma expressão qualquer que use parenteses. seu aplicativo deverá
 # analisar se a expressão passada está com os parenteses abertos e fechados na ordem correta.
+
+parenteses = 0
+exp = str(input('digite uma expressão com parenteses: ')).strip()
+for c, u in enumerate(exp):
+    if u in '(':
+        parenteses += 1
+    elif u in ')':
+        parenteses -= 1
+if parenteses == 0:
+    print('o número de parenteses esta correto')
+elif parenteses > 0:
+    print('esta faltando fechar um ou mais parenteses')
+elif parenteses < 0:
+    print('esta sobrando um ou mais parenteses')
